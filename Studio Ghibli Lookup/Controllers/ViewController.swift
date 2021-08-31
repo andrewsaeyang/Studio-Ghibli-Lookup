9e16f1017a78b2e46fad35aade7abb18bfb047ad
Andrew Saeyang <a09saeyang@gmail.com>
2021-08-31 16:31:41 -0700

    Basic file structure with cloudkit

---
+++ Studio Ghibli Lookup/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Studio Ghibli Lookup
+//
+//  Created by Andrew Saeyang on 8/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Studio Ghibli Lookup/Models/PlaceHolder.swift
@@ -0,0 +1,8 @@
+//
+//  PlaceHolder.swift
+//  Studio Ghibli Lookup
+//
+//  Created by Andrew Saeyang on 8/31/21.
+//
+
+import Foundation
