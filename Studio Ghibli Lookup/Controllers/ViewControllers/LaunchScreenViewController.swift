6317537c48b9c7a932e12c1e674c7a39b7dccd57
Andrew Saeyang <a09saeyang@gmail.com>
2021-10-27 00:54:47 -0700

    added LaunchScreen image

---
+++ Studio Ghibli Lookup/Controllers/ViewControllers/LaunchScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LaunchScreenViewController.swift
+//  Studio Ghibli Lookup
+//
+//  Created by Andrew Saeyang on 10/27/21.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
